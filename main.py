77a916a18b36181ad5b4f593daf03b7cd79aae0f
HarukaB <bo2077@proton.me>
2023-05-11 13:02:48 -0400

    重构

--- main.py
+++ main.py
@@ -7,81 +7,79 @@ from datetime import datetime
 import sys
 import zipfile
 
-METADATA_URL = 'https://nwo.ink/metadata.json'
-#update_file的文件名在metadata.json中updfilename字段定义,需要从中读取
-UPDATE_FILE = with open('metadata.json', 'r') as f:
-    metadata = json.load(f)
-    UPDATE_FILE = metadata['updfilename']
+# URL for downloading metadata
+METADATA_URL = 'https://themea.eu.org/update/metadata.json'
+# Name of the file with the game version
+GAME_VERSION_FILE = 'gamever.txt'
 
 def download_file(url, filename):
     try:
-        r = requests.get(url)
-        r.raise_for_status()
-        with open(filename, 'wb') as f:
-            f.write(r.content)
-    except requests.RequestException as e:
-        raise SystemExit(f"Error downloading file: {e}")
+        response = requests.get(url)
+        response.raise_for_status()
+        with open(filename, 'wb') as out_file:
+            out_file.write(response.content)
+    except requests.RequestException as err:
+        print(f"Failed to download file from {url}. Error: {err}")
+        sys.exit(1)
 
-def load_metadata(filename):
+def load_json(filename):
     try:
-        with open(filename, 'r') as f:
-            return json.load(f)
-    except FileNotFoundError as e:
-        raise SystemExit(f"Error loading metadata: {e}")
+        with open(filename, 'r') as in_file:
+            return json.load(in_file)
+    except FileNotFoundError as err:
+        print(f"Failed to load json file {filename}. Error: {err}")
+        sys.exit(1)
 
-def check_update(game_version, new_version):#检查元数据中latestversioncode是不是比本地game_version.txt中的版本号大
-    return new_version > game_version
+def check_update(version, latest_version):
+    return int(latest_version) > int(version)
 
-def check_updatesum(filename, sha256):
-    with open(filename, 'rb') as f:
-        file_sha256 = hashlib.sha256(f.read()).hexdigest()
+def check_sha256(filename, sha256):
+    with open(filename, 'rb') as in_file:
+        file_sha256 = hashlib.sha256(in_file.read()).hexdigest()
     return file_sha256 == sha256
 
-def unzip_update(filename, target_dir):
+def unzip_file(filename, path):
     with zipfile.ZipFile(filename, 'r') as zip_ref:
-        zip_ref.extractall(target_dir)
+        zip_ref.extractall(path)
 
-def replace_files(source_dir, target_dir):
-    shutil.rmtree(target_dir, ignore_errors=True)
-    shutil.copytree(source_dir, target_dir)
+def replace_files(source_path, dest_path):
+    shutil.rmtree(dest_path, ignore_errors=True)
+    shutil.copytree(source_path, dest_path)
 
 def main():
-    # Load local game version
-    try:
-        with open('game_version.txt', 'r') as f:
-            game_version = f.read()
-    except FileNotFoundError as e:
-        raise SystemExit(f"Error loading game version: {e}")
-
     # Download and load metadata
     download_file(METADATA_URL, 'metadata.json')
-    metadata = load_metadata('metadata.json')
+    metadata = load_json('metadata.json')
 
-    # Check for update
-    if not check_update(game_version, metadata['VERSION']):
-        print('No update available')
-        return
+    # Load local game version
+    game_version = load_json(GAME_VERSION_FILE)
 
-    # Download update
-    download_file(metadata['DLINK'], UPDATE_FILE)
+    # Check for updates
+    if not check_update(game_version['version'], metadata['latestversioncode']):
+        print("No new updates.")
+        sys.exit(0)
 
-    # Check update integrity
-    if not check_updatesum(UPDATE_FILE,metadata['PACKSHA256']):
-        raise SystemExit("Update package failed integrity check")
+    # Download the update
+    update_file = metadata['updfilename']
+    download_file(metadata['1drv'], update_file)
 
-    # Extract update
-    unzip_update(UPDATE_FILE, 'update')
+    # Check the update package
+    if not check_sha256(update_file, metadata['SHA256']):
+        print("Update package integrity check failed.")
+        sys.exit(1)
 
-    # Replace files
-    replace_files('update', '.')
+    # Extract the update
+    unzip_file(update_file, './update')
 
-    # Delete update files
-    shutil.rmtree('update', ignore_errors=True)
-    os.remove(UPDATE_FILE)
+    # Replace the old files with the new ones
+    replace_files('./update', './')
 
-    # Update version file
-    with open('game_version.txt', 'w') as f:
-        f.write(metadata['VERSION'])
+    # Update the local game version
+    game_version['version'] = metadata['latestversioncode']
+    with open(GAME_VERSION_FILE, 'w') as out_file:
+        json.dump(game_version, out_file)
+
+    print("Update complete.")
 
 if __name__ == "__main__":
     main()
