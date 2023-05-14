12ff5f1b2daee12da409288c73d70c58f121298a
HarukaB <bo2077@proton.me>
2023-05-14 10:24:19 -0400

    修复不能正确检测更新的BUG

--- main.py
+++ main.py
@@ -37,7 +37,33 @@ METADATA_URL = 'https://themea.eu.org/update/metadata.json'
 # Name of the file with the game version
 GAME_VERSION_FILE = 'gamever.txt'
 CALLBACK_URL = 'https://themea.eu.org/callback'
+def set_game_or_updater_version(input, line):  #第一个参数是要写入的内容，第二个参数是要写入的行数(1,2)
+    try:
+        with open(GAME_VERSION_FILE, 'r') as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        lines = ['0\n', '0\n']
 
+    if len(lines) < line:
+        lines.extend(['0\n'] * (line - len(lines)))
+
+    lines[line - 1] = str(input) + '\n'
+
+    with open(GAME_VERSION_FILE, 'w') as file:
+        file.writelines(lines)
+
+def get_game_or_updater_version(line):  #line参数是要读取的行数(1,2)
+    while True:
+        try:
+            with open(GAME_VERSION_FILE, 'r') as file:
+                if line == 1:
+                    return int(file.readline().strip())
+                elif line == 2:
+                    file.readline()
+                    return int(file.readline().strip())
+        except (FileNotFoundError, ValueError):
+            set_game_or_updater_version('0', 1)
+            set_game_or_updater_version('0', 2)
 def init_log():  #创建颜色输出函数
     logger = logging.getLogger('ROOT')
     logger.setLevel(logging.DEBUG)
@@ -78,16 +104,6 @@ def create_rename_bat():
         file.write('del callback_info.json\n')
         file.write('rename updatertmp.exe updater.exe\n')
         file.write('exit\n')
-def load_game_version():
-    try:
-        with open(GAME_VERSION_FILE, 'r') as in_file:
-            return int(in_file.read().strip())
-    except FileNotFoundError as err:
-        # print(f"游戏版本文件未找到,正在创建并设置版本为0.")
-        log.info(f"游戏版本文件未找到,正在创建并设置版本为0.")
-        with open(GAME_VERSION_FILE, 'w') as out_file:
-            out_file.write('0')
-        return 0
 def write_file(filename, line_number, content):
     with open(filename, 'r') as in_file:
         lines = in_file.readlines()
@@ -120,8 +136,6 @@ def download_file(url, filename):
                     out_file.write(chunk)
             progress_bar.close()
     except requests.RequestException as err:
-        # print(f"Failed to download file from {url}. Error: {err}")
-        # print(f"下载文件失败. 错误")
         log.error(f"从{url}下载文件失败. 错误：{err}")
         return False
     return True
@@ -130,8 +144,6 @@ def check_files():
 
     for file in files_to_check:
         if not os.path.isfile(file):
-            # print(f"Error: {file} not found. Please place the program in the game root directory.")
-            # print(f"请将程序放在游戏根目录下.")
             log.error(f"请将程序放在游戏根目录下.")
             time.sleep(10)
             sys.exit(1)
@@ -140,25 +152,17 @@ def download_update(metadata, dest_path='./'):
     urls = [metadata['1drv'], metadata['1drvback'], metadata['github']]
     for url in urls:
         try:
-            # print(f"Trying to download file from {url}...")
-            # print(f"尝试下载文件...")
             log.info(f"尝试从{url}下载文件...")
             obj = SmartDL(url, dest=dest_path)
             obj.start()
             while not obj.isFinished():
                 progress_bar = tqdm(total=obj.filesize, unit='B', unit_scale=True)
                 progress_bar.update(obj.get_dl_size()) # type: ignore
-            # print(f"\nDownloaded file {obj.get_dest()}")
             log.info(f"\n下载完成： {obj.get_dest()}")
-            # print(f"下载文件成功")
             return url  # 返回使用的 URL
         except Exception as err:
-            # print(f"Failed to download file from  {url}.Error: {err}")
-            # print(f"尝试下载文件失败. 错误")
             log.error(f"尝试从{url}下载文件失败. 错误：{err}")
             continue
-    # print("Failed to download file from all URLs.")
-    # print("所有下载途径均失败，请使用手动更新")
     log.warning("所有下载途径均失败，请使用手动更新")
     return None  # 如果所有的 URL 都失败了，返回 None
 def get_public_ip():
@@ -170,7 +174,6 @@ def get_public_ip():
         else:
             return None
     except Exception as e:
-        # print(f"Failed to get public IP. Error: {e}")
         log.error(f"获取IP失败，错误： {e}")
         return None
 def load_json(filename):
@@ -178,8 +181,6 @@ def load_json(filename):
         with open(filename, 'r') as in_file:
             return json.load(in_file)
     except FileNotFoundError as err:
-        # print(f"Failed to load json file {filename}. Error: {err}")
-        # print("加载json文件失败. 错误: {err}")
         log.error("加载文件{filename}失败. 错误: {err}")
         callback_info['status'] = "Failed to load json file"
         write_callback_info(callback_info)
@@ -196,7 +197,6 @@ def check_sha256(filename, sha256):
 def update_updater():
     metadata = load_json(METADATA_FILE)
     if 'updaterversion' not in metadata:
-        # print('没有在元数据中找到更新器版本号,请检查元数据文件是否正确.')
         log.error('没有在元数据中找到更新器版本号,请检查元数据文件是否正确.')
         return
 def get_local_updater_version():
@@ -252,12 +252,14 @@ def update_self(metadata):
         with open(gamever_filename, 'r') as file:
             lines = file.readlines()
         if len(lines) >= 2 and lines[1].strip().isdigit():
-            local_updater_version = int(lines[1].strip())
+            with open(gamever_filename, 'r') as file:
+                file.readline()
+                second_line = file.readline().strip()
+            local_updater_version = int(second_line)
 
         remote_updater_version = int(metadata['updaterversion'])
 
-        if remote_updater_version > local_updater_version:
-            # print("更新器发现新的更新!")  
+        if remote_updater_version > local_updater_version: 
             log.info("更新器发现新的更新!")# 在检测到有新的更新时，输出一个提示语
             for download_url in [metadata['updatergitee'], metadata['updater1drv'], metadata['updaterbackup']]:
                 try:
@@ -285,10 +287,9 @@ def update_self(metadata):
                                     lines = file.readlines()
                                 lines[1] = str(metadata['updaterversion']) + '\n'
                                 with open(gamever_filename, 'w') as file:
-                                    file.writelines(lines)
+                                    file.write(str(metadata['updaterversion']) + '\n')
                                 callback_info['compeleteupdater'] = 'True'
                                 send_callback(CALLBACK_URL, callback_info)
-                                # print("更新器马上更新完毕，弹出命令窗口是正常现象。")
                                 log.info("更新器马上更新完毕，弹出命令窗口是正常现象。")
                                 return  # 添加此行以在成功更新后返回
                         else:
@@ -297,10 +298,8 @@ def update_self(metadata):
                     else:
                         raise Exception('下载失败.')
                 except Exception as e:
-                    # print('Failed to update updater from ' + download_url + '. Reason: ' + str(e))
                     log.error('从 ' + download_url + '更新更新器失败. 原因： ' + str(e))
         else:
-            # print('No need to update.')
             log.info('更新器无需更新.')
 def main():
     check_files()
@@ -312,7 +311,8 @@ def main():
     metadata = load_json('metadata.json')
 
     # Load local game version
-    game_version = load_update_or_create_file(GAME_VERSION_FILE, 1)
+    with open (GAME_VERSION_FILE,'r') as file:
+        game_version = file.read().strip()
     callback_info = {
         'clientip': get_public_ip(),
         'clientdepotver': game_version,
@@ -327,9 +327,7 @@ def main():
     write_callback_info(callback_info)
 
     # Check for updates
-    if not check_update(game_version, metadata['latestversioncode']):
-        # print("No new updates, but updater may need to be updated.")
-        # print("游戏没有新的更新,但是更新器可能需要更新，如果没有反应请勿在5分钟内关闭程序.")
+    if not check_update(get_game_or_updater_version(1), metadata['latestversioncode']):
         log.info("游戏没有新的更新,但是更新器可能需要更新，如果没有反应请勿在5分钟内关闭程序.")
         callback_info['status'] = "No new updates"
         write_callback_info(callback_info)
@@ -337,8 +335,6 @@ def main():
         update_self(metadata)
         sys.exit(0)
     
-    # print("New update available. Checking for update package...")
-    # print("新的更新可用,正在检查更新包...")
     log.info("新的更新可用,正在检查更新包...")
     # Prepare for update
     update_file = metadata['updfilename']
@@ -346,18 +342,15 @@ def main():
 
     # Check if update file already exists
     if os.path.exists(update_file) and check_sha256(update_file, metadata['SHA256']):
-        # print("Update package exists and integrity check passed.")
-        # print("更新包已存在且完整性检查通过.")
         log.info("更新包已存在且完整性检查通过.")
     else:
-        # print("Update package not found or integrity check failed. Downloading...")
-        # print("更新包未找到或完整性检查失败. 正在下载...()")
-        log.info("更新包未找到或完整性检查失败. 正在下载...()")
-        os.remove(metadata['updfilename'])  # If the file exists but integrity check failed,remove it
+        log.info("更新包未找到或完整性检查失败. 正在下载...")
+        try:
+            os.remove(metadata['updfilename']) 
+        except:
+            pass
         download_url = download_update(metadata, dest_path="./" + update_file)  # 获取使用的 URL
         if download_url is None:
-            # print("Update download failed.")
-            # print("更新下载失败(所有url都失败,请使用手动更新)")
             log.warning("更新下载失败(所有url都失败,请使用手动更新)")
             callback_info['status'] = "Failed to download update from all URLs."
             write_callback_info(callback_info)
@@ -367,8 +360,6 @@ def main():
     callback_info['downloadfrom'] = download_url if download_url else "Existing file"
     write_callback_info(callback_info)
     
-    # print("Update package integrity check passed. Extracting...")
-    # print("更新包完整性检查通过,正在解压...")
     log.info("更新包完整性检查通过,正在解压...") 
     unzip_file(update_file, './update')
     
@@ -376,13 +367,7 @@ def main():
     replace_files('update', '.')
 
     # Update the local game version
-    game_version = metadata['latestversioncode']
-    with open(GAME_VERSION_FILE, 'w') as out_file:
-        out_file.write(str(game_version))
-
-    # Update the updater version
-    load_update_or_create_file(GAME_VERSION_FILE, 2, metadata['updaterversion'])
-    # Update the updater itself if needed
+    set_game_or_updater_version(metadata['latestversioncode'],0)
     write_callback_info(callback_info)
     os.remove('metadata.json')
     os.remove(update_file)
@@ -390,9 +375,10 @@ def main():
     callback_info['status'] = "Complete"
     callback_info['compeleteupdater']='Compeleted All Steps,But unsure if the updater is updated.'
     send_callback(CALLBACK_URL, callback_info)
-    # print("Update complete.")
-    # print("更新完成")
-    log.info("更新完成")
+    # Update the updater itself if needed
+    log.info("游戏更新完成，但更新器自己可能需要更新，请在5分钟内不要关闭程序.")
+    # Update the updater version
+    set_game_or_updater_version(metadata['updaterversion'], 2)
     update_self(metadata)
 if __name__ == "__main__":
     main()
